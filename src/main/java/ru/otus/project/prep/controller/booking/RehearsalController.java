52925c57447ccc1c79a4ddc3588b88056b527176
myeyeswereclosed <belovi-tech@mail.ru>
2020-10-18 16:55:44 +0300

    project-prep ui taken

--- src/main/java/ru/otus/project/prep/controller/booking/RehearsalController.java
+++ src/main/java/ru/otus/project/prep/controller/booking/RehearsalController.java
@@ -1,5 +1,7 @@
 package ru.otus.project.prep.controller.booking;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.otus.project.prep.dto.RehearsalDto;
 import ru.otus.project.prep.dto.RoomDto;
 
@@ -7,11 +9,19 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
+@Controller
 public class RehearsalController {
     // get all reservations made for this week (from -> to) in some Room
     public List<RehearsalDto> getFree(RoomDto room, LocalDateTime fromDate, LocalDateTime toDate) {
         return Collections.emptyList();
     }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+
 
 //    // get all reservations made by band/musician
 //    public List<BookingDto> getAll(Artist user);
