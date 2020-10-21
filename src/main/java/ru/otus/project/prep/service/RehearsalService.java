d5aa3c8e849952f7249c29d22850277a4e042f51
myeyeswereclosed <belovi-tech@mail.ru>
2020-10-21 20:08:41 +0300

    project-prep fixes

--- src/main/java/ru/otus/project/prep/service/RehearsalService.java
+++ src/main/java/ru/otus/project/prep/service/RehearsalService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface RehearsalService {
+    Rehearsal reserve(Rehearsal rehearsal);
+
     List<Rehearsal> getReservedFromDate(int roomId, String date);
 
     Optional<Rehearsal> cancel(long rehearsalId) throws TooLateToCancel;
