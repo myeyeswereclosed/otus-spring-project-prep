df1653fb91df2dca8f6f4f621bf8500f4f0c0b7a
myeyeswereclosed <belovi-tech@mail.ru>
2020-10-31 11:09:14 +0300

    oauth index page

--- gateway/src/main/java/ru/otus/project/gateway/controller/RoomController.java
+++ gateway/src/main/java/ru/otus/project/gateway/controller/RoomController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.otus.project.gateway.config.Timetable;
 
-
 @Controller
 @RequiredArgsConstructor
 public class RoomController {
@@ -15,14 +14,12 @@ public class RoomController {
 
     @GetMapping("/room/{id}/timetable")
     public String roomTimetable(Model model, @PathVariable String id) {
-        var timetableStart = timetable.getStart();
-        var timetableEnd = timetable.getEnd();
-
-//        model.addAttribute("artist", artist);
-        model.addAttribute("roomId", id);
-        model.addAttribute("timetableStart", timetableStart);
-        model.addAttribute("timetableEnd", timetableEnd);
-        model.addAttribute("timetablePeriod", timetable.getPeriod());
+        model
+            .addAttribute("roomId", id)
+            .addAttribute("timetableStart", timetable.getStart())
+            .addAttribute("timetableEnd", timetable.getEnd())
+            .addAttribute("timetablePeriod", timetable.getPeriod())
+        ;
 
         return "timetable";
     }
